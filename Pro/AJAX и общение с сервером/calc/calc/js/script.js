60802bfb8784a2be8ac087f74e12d2bfebb644de
muratbek <muratbek@example.com>
2022-09-07 18:30:44 +0600

    dop

--- Pro/AJAX и общение с сервером/calc/calc/js/script.js
+++ Pro/AJAX и общение с сервером/calc/calc/js/script.js
@@ -1,6 +1,27 @@
 'use strict';
 
 const inputSom = document.querySelector('#som'),
-    inputUsd = document.querySelector('$usd');
+    inputUsd = document.querySelector('#usd');
 
-    
+inputSom.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json'); //method, url, async, login, password
+    request.setRequestHeader('Content-type','application/json; charset=utf-8');
+    request.send();
+ 
+    request.addEventListener('load', () => {
+        if(request.status === 200){
+            //console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputSom.value / data.current.usd).toFixed(2);
+        }else{
+            inputUsd.value = 'что-то пошло не так';
+        }
+    });
+
+    //status
+    //statusText
+    //response
+    //ready
+})
